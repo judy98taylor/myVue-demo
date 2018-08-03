d8d40c6bb161d4ef699096c2edf53af1149d34b8
zxd <zd9027@gmail.com>
2018-08-03 17:49:33 +0800

    build

---
+++ demo/dist/precache-manifest.9c0a821b30696964cec270cac059aee7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "abf9c05c80af9d4929dc",
+    "url": "/css/chunk-vendors.7a2693b7.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1e2e8f5cb11cffb8988c",
+    "url": "/js/toupiao.1e2e8f5c.js"
+  },
+  {
+    "revision": "abf9c05c80af9d4929dc",
+    "url": "/js/chunk-vendors.abf9c05c.js"
+  },
+  {
+    "revision": "0161eee70fbb04a52331",
+    "url": "/js/choujiang.0161eee7.js"
+  },
+  {
+    "revision": "17f085f5a792cafaa927",
+    "url": "/js/app.17f085f5.js"
+  },
+  {
+    "revision": "29f6226db2b81c32d0ae",
+    "url": "/js/about.29f6226d.js"
+  },
+  {
+    "revision": "9557313fe2339a69fa50486cc22020e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2e8f5cb11cffb8988c",
+    "url": "/css/toupiao.7bd5a4eb.css"
+  },
+  {
+    "revision": "0161eee70fbb04a52331",
+    "url": "/css/choujiang.f1fb4749.css"
+  },
+  {
+    "revision": "17f085f5a792cafaa927",
+    "url": "/css/app.b8346608.css"
+  }
+];
