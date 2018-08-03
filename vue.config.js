f60bfd6d908c117b76bcaa584ed664bbd26885da
zxd <zd9027@gmail.com>
2018-08-03 19:10:49 +0800

    -s

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 module.exports = {
   // 选项...
-  baseUrl: process.env.NODE_ENV === "default" ? "/myVue-demo/dist/" : "/"
+  baseUrl: "/myVue-demo/dist/"
+  //   assetsDir: "."
 };
